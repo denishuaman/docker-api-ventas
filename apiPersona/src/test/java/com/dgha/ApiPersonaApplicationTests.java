7ddd781f142d7469beb7fa1c4450409ab82a9aea
Denis <denis07.1993@gmail.com>
2019-08-01 00:19:43 -0500

    Creación de servicios

---
+++ apiPersona/src/test/java/com/dgha/ApiPersonaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dgha;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiPersonaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
