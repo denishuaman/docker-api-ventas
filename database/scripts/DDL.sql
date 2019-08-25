a44eb803a19e3ff524d39111b7e1e97176be15ec
Denis Huamán <denis07.1993@gmail.com>
2019-08-25 18:56:58 -0500

    Cambio de base de datos a postgresql

--- database/scripts/DDL.sql
+++ database/scripts/DDL.sql
@@ -1,62 +1,45 @@
-CREATE DATABASE IF NOT EXISTS ventas CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `ventas` ;
+drop table if exists detalle_venta;
+drop table if exists venta;
+drop table if exists persona;
+drop table if exists producto;
 
-SET foreign_key_checks = 0;
+create table public.producto (
+  id_producto serial primary key,
+  nombre varchar(100) not null,
+  marca varchar(100) not null,
+  precio float not null
+);
 
-DROP TABLE IF EXISTS producto;
-DROP TABLE IF EXISTS persona;
-DROP TABLE IF EXISTS venta;
-DROP TABLE IF EXISTS detalle_venta;
+create table persona (
+  id_persona serial primary key,
+  dni varchar(8) not null unique,
+  nombres varchar(100) not null,
+  apellidos varchar(100) not null
+);
 
-SET foreign_key_checks = 1;
+create table venta (
+  id_venta serial primary key,
+  id_persona integer not null,
+  importe float not null,
+  fecha timestamp not null,
 
-CREATE TABLE producto (
-  `id_producto` BIGINT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(100) NOT NULL,
-  `marca` VARCHAR(100) NOT NULL,
-  `precio` DOUBLE NOT NULL,
-  PRIMARY KEY (`id_producto`))
-ENGINE = InnoDB;
+  constraint fk_venta_persona_idx foreign key (id_persona)
+    references public.persona(id_persona) match simple
+      on update no action
+      on delete no action
+);
 
-CREATE TABLE persona (
-  `id_persona` BIGINT NOT NULL AUTO_INCREMENT,
-  `dni` VARCHAR(8) NOT NULL,
-  `nombres` VARCHAR(100) NOT NULL,
-  `apellidos` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`id_persona`) ,
-  UNIQUE INDEX `dni_UNIQUE` (`dni` ASC)  )
-ENGINE = InnoDB;
-
-CREATE TABLE venta (
-  `id_venta` BIGINT NOT NULL AUTO_INCREMENT,
-  `id_persona` BIGINT NOT NULL,
-  `importe` DOUBLE NOT NULL,
-  `fecha` TIMESTAMP NOT NULL,
-  PRIMARY KEY (`id_venta`) ,
-  INDEX `fk_venta_persona_idx` (`id_persona` ASC) ,
-  CONSTRAINT `fk_venta_persona`
-    FOREIGN KEY (`id_persona`)
-    REFERENCES `ventas`.`persona` (`id_persona`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS detalle_venta (
-  `id_detalle_venta` BIGINT NOT NULL AUTO_INCREMENT,
-  `id_venta` BIGINT NOT NULL,
-  `id_producto` BIGINT NOT NULL,
-  `cantidad` INT NULL,
-  INDEX `fk_venta_has_producto_producto1_idx` (`id_producto` ASC) ,
-  INDEX `fk_venta_has_producto_venta1_idx` (`id_venta` ASC) ,
-  PRIMARY KEY (`id_detalle_venta`) ,
-  CONSTRAINT `fk_venta_has_producto_venta1`
-    FOREIGN KEY (`id_venta`)
-    REFERENCES `ventas`.`venta` (`id_venta`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_venta_has_producto_producto1`
-    FOREIGN KEY (`id_producto`)
-    REFERENCES `ventas`.`producto` (`id_producto`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+create table if not exists detalle_venta (
+  id_detalle_venta serial primary key,
+  id_venta int not null,
+  id_producto int not null,
+  cantidad int null,
+  constraint fk_venta_has_producto_producto1_idx foreign key (id_producto)
+    references public.producto(id_producto) match simple
+      on update no action
+      on delete no action,
+  constraint fk_venta_has_producto_venta1_idx foreign key (id_venta)
+    references public.venta(id_venta) match simple
+      on update no action
+      on delete no action
+);
