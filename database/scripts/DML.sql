1b7b877f9ac16e1ce8ee4487ab67c5e799b54376
Denis Huamán <denis07.1993@gmail.com>
2019-08-30 22:31:08 -0500

    Actualización de inserts en database/scripts/DML.sql

--- database/scripts/DML.sql
+++ database/scripts/DML.sql
@@ -7,8 +7,8 @@ INSERT INTO producto(nombre, marca, precio) VALUES('Memoria RAM','Kingston',230)
 INSERT INTO producto(nombre, marca, precio) VALUES('Cargador','Toshiba',50);
 INSERT INTO producto(nombre, marca, precio) VALUES('Laptop','SONY VAIO',3400);
 
-INSERT INTO venta(id_persona, importe, fecha) VALUES(1,191.5,current_timestamp);
-INSERT INTO venta(id_persona, importe, fecha) VALUES(2,510,current_timestamp);
+INSERT INTO venta(id_persona, importe, fecha) VALUES(1,191.5,'2019-08-25 22:36:01.565');
+INSERT INTO venta(id_persona, importe, fecha) VALUES(2,510,'2019-08-26 22:36:01.565');
 
 INSERT INTO detalle_venta(id_venta, id_producto, cantidad) VALUES(1,1,3);
 INSERT INTO detalle_venta(id_venta, id_producto, cantidad) VALUES(1,2,5);
